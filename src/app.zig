0618d96006837143f6628d275d431fb6092fdbd7
pchchv <ipchchv@gmail.com>
2024-07-29 12:47:08 +0300

    Implement a new method

--- src/app.zig
+++ src/app.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const helpers = @import("helpers.zig");
+const EditableText = helpers.EditableText;
 const Line = struct {
     start: usize = 0,
     end: usize = 0,
@@ -5,4 +8,14 @@ const Line = struct {
 
 pub const App = struct {
     const Self = @This();
+    typed: EditableText,
+
+    pub fn new(allocator: *std.mem.Allocator, arena: *std.mem.Allocator) Self {
+        return Self{
+            .allocator = allocator,
+            .arena = arena,
+            .typed = EditableText.init(allocator),
+            .lines = std.ArrayList(Line).init(allocator),
+        };
+    }
 };
