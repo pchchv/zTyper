7b0300fa2fcf3404c5c26327630ff9d3cc3f5539
pchchv <ipchchv@gmail.com>
2024-07-29 12:35:17 +0300

    Implement lerp and unlerp methods

--- src/helpers.zig
+++ src/helpers.zig
@@ -13,4 +13,23 @@ pub const Vector2 = struct {
     const Self = @This();
     x: f32 = 0.0,
     y: f32 = 0.0,
+
+    pub fn lerp(v1: Vector2, v2: Vector2, t: f32) Vector2 {
+        return Vector2{
+            .x = lerpf(v1.x, v2.x, t),
+            .y = lerpf(v1.y, v2.y, t),
+        };
+    }
+
+    /// Suppose that v lies along the line v1-v2 (may be outside the segment).
+    /// Then do not check both x and y unlerp.
+    /// Just return the first value found.
+    pub fn unlerp(v1: Vector2, v2: Vector2, v: Vector2) f32 {
+        if (v1.x != v2.x) {
+            return unlerpf(v1.x, v2.x, v.x);
+        } else if (v1.y != v2.y) {
+            return unlerpf(v1.y, v2.y, v.y);
+        }
+        return 0;
+    }
 };
