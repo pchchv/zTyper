75cce0623770faed3b40f51382d249f3615a5888
pchchv <ipchchv@gmail.com>
2024-07-29 12:27:34 +0300

    Implement lerpf and unlerpf funcs

--- src/helpers.zig
+++ src/helpers.zig
@@ -1,3 +1,14 @@
+pub fn lerpf(start: f32, end: f32, t: f32) f32 {
+    return (start * (1.0 - t)) + (end * t);
+}
+
+pub fn unlerpf(start: f32, end: f32, t: f32) f32 {
+    if ((end == t) or (end <= start)) {
+        return 1.0;
+    }
+    return (t - start) / (end - start);
+}
+
 pub const Vector2 = struct {
     const Self = @This();
     x: f32 = 0.0,
