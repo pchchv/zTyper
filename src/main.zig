27844cb523c0092303b286c29c92779d667017ed
pchchv <ipchchv@gmail.com>
2024-07-29 12:02:49 +0300

    Init SDL

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
+const c = @import("c.zig");
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
     defer {
         _ = gpa.deinit();
     }
+
+    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
 }
