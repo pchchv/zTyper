285f3d5ad4dd431a6f60c4e9aa2b2cd0d724b070
pchchv <ipchchv@gmail.com>
2024-07-30 10:29:26 +0300

    Implement a Glyph

--- src/glyphee.zig
+++ src/glyphee.zig
@@ -1,4 +1,16 @@
+const c = @import("c.zig");
+const helpers = @import("helpers.zig");
+
+const Vector4_gl = helpers.Vector4_gl;
 const DEFAULT_FONT: FontType = .debug;
+
+const Glyph = struct {
+    char: u8,
+    font: FontType,
+    color: Vector4_gl,
+    quad: c.stbtt_aligned_quad,
+    z: f32,
+};
 
 pub const FontType = enum {
     /// Font for debug purposes and things.
