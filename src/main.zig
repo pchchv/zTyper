233edabd99b3824e56107e11edd0a7014c46bd11
pchchv <ipchchv@gmail.com>
2024-08-05 14:18:54 +0300

    Implement an app rendering

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const c = @import("c.zig");
 const App = @import("app.zig").App;
+const Renderer = @import("renderer.zig").Renderer;
 
 pub fn main() anyerror!void {
+    // TODO (14 Jul 2021 sam): Figure out how to handle this safety flag.
     var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
     defer {
         _ = gpa.deinit();
@@ -15,11 +17,12 @@ pub fn main() anyerror!void {
     defer c.SDL_Quit();
 
     const start_ticks = c.SDL_GetTicks();
-
     var loading_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     var app = App.new(&gpa.allocator, &loading_arena.allocator);
     try app.init();
     defer app.deinit();
+    var renderer = try Renderer.init(&app.typesetter, &app.camera, &gpa.allocator, "typeroo");
+    defer renderer.deinit();
     loading_arena.deinit();
     const init_ticks = c.SDL_GetTicks();
     std.debug.print("app init complete in {d} ticks\n", .{init_ticks - start_ticks});
@@ -32,10 +35,10 @@ pub fn main() anyerror!void {
                 else => app.handle_inputs(event),
             }
         }
-
         var frame_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
         const ticks = c.SDL_GetTicks();
         app.update(ticks, &frame_allocator.allocator);
+        renderer.render_app(ticks, &app);
         frame_allocator.deinit();
         app.reset_inputs();
     }
