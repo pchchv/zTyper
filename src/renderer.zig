12bfde278b496696de5a3ce216547978f1c831e6
pchchv <ipchchv@gmail.com>
2024-08-05 13:55:56 +0300

    Implement a Renderer

--- src/renderer.zig
+++ src/renderer.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 const c = @import("c.zig");
 const helpers = @import("helpers.zig");
+const glyph_lib = @import("glyphee.zig");
 
+const Camera = helpers.Camera;
 const Vector2_gl = helpers.Vector2_gl;
 const Vector3_gl = helpers.Vector3_gl;
 const Vector4_gl = helpers.Vector4_gl;
+const TypeSetter = glyph_lib.TypeSetter;
 
 const VertexData = struct {
     position: Vector3_gl = .{},
@@ -37,4 +40,21 @@ const ShaderData = struct {
         self.triangle_verts.shrinkRetainingCapacity(0);
         self.indices.shrinkRetainingCapacity(0);
     }
+};
+
+pub const Renderer = struct {
+    const Self = @This();
+    ticks: u32 = 0,
+    camera: *Camera,
+    vao: c.GLuint = 0,
+    vbo: c.GLuint = 0,
+    ebo: c.GLuint = 0,
+    z_val: f32 = 0.999,
+    window: *c.SDL_Window,
+    base_shader: ShaderData,
+    text_shader: ShaderData,
+    typesetter: *TypeSetter,
+    renderer: *c.SDL_Renderer,
+    gl_context: c.SDL_GLContext,
+    allocator: *std.mem.Allocator,
 };
