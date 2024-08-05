5e73e1f3415da5e55f6d54f71c1ffd56eb6f020b
pchchv <ipchchv@gmail.com>
2024-08-05 13:50:23 +0300

    Implement a ShaderData

--- src/renderer.zig
+++ src/renderer.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+const c = @import("c.zig");
 const helpers = @import("helpers.zig");
 
 const Vector2_gl = helpers.Vector2_gl;
@@ -8,4 +10,14 @@ const VertexData = struct {
     position: Vector3_gl = .{},
     texCoord: Vector2_gl = .{},
     color: Vector4_gl = .{},
+};
+
+const ShaderData = struct {
+    const Self = @This();
+    program: c.GLuint = 0,
+    texture: c.GLuint = 0,
+    has_tris: bool = true,
+    has_lines: bool = false,
+    indices: std.ArrayList(c_uint),
+    triangle_verts: std.ArrayList(VertexData),
 };
